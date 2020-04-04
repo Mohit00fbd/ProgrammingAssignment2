fba01ac3bf9d3cc815f7461c1e35dbbffcef22b6
Mradul Verma <mohit00fbd@gmail.com>
2020-04-04 19:31:47 +0530

    Assignment Completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here, we learn using the <<- operator which is used to 
+## assign value to a different environment from current
+## environment. The following two functions are used to
+## compute and cache the inverse of matrices.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing a function
+## to set the matrix, get the matrix, set the inverse, and
+## get the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y){
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) inverse <<- inv
+    getinverse <- function() inverse
+    list(set=set, get=get, setinverse = setinverse,
+         getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## The following function calculates inverse of the special
+## matrix created with makeCacheMatrix function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse
+    if(!is.null(inverse)){
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
